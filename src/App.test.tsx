0f5ee01f58ebaa8834976729fa2840eec8ccae4c
Alikhan <muhammedalikhan27@gmail.com>
2026-04-14 08:27:45 +0530

    feat: Added the hour block in the App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -50,3 +50,44 @@ describe("App component - Seconds Lamp", () => {
     expect(lampElement).toHaveClass("yellow");
   });
 });
+
+describe("App component - Hours Lamp", () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+  it("renders the App component and checks if the hour section TimeBlock components have correct number of lamps", () => {
+    render(<App />);
+    const hourSection = screen.getByRole("hours");
+    const timeBlocks = hourSection.querySelectorAll("[role='time-block']");
+    timeBlocks.forEach((timeBlock) => {
+      const lamps = timeBlock.querySelectorAll("[role='lamp']");
+      expect(lamps.length).toBe(4); // Each TimeBlock should have 4 lamps
+    });
+  });
+  it("renders the App component and checks if the hour section TimeBlock components have correct number of lit lamps based on time", () => {
+    const testDate = new Date(2026, 0, 1, 13, 0, 0);
+    vi.setSystemTime(testDate);
+    render(<App />);
+    const hourSection = screen.getByRole("hours");
+    const timeBlocks = hourSection.querySelectorAll("[role='time-block']");
+    const firstTimeBlockLamps = timeBlocks[0].querySelectorAll("[role='lamp']");
+
+    // - (5-hour blocks) to have 2 lit lamps (10 hours)
+    expect(firstTimeBlockLamps[0]).toHaveClass("red");
+    expect(firstTimeBlockLamps[1]).toHaveClass("red");
+    expect(firstTimeBlockLamps[2]).not.toHaveClass("red");
+    expect(firstTimeBlockLamps[3]).not.toHaveClass("red");
+
+    // - (1-hour blocks) to have 3 lit lamps (3 hours)
+    const secondTimeBlockLamps =
+      timeBlocks[1].querySelectorAll("[role='lamp']");
+    expect(secondTimeBlockLamps[0]).toHaveClass("red");
+    expect(secondTimeBlockLamps[1]).toHaveClass("red");
+    expect(secondTimeBlockLamps[2]).toHaveClass("red");
+    expect(secondTimeBlockLamps[3]).not.toHaveClass("red");
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
 import "./App.css";
 import Lamp from "./components/Lamp/Lamp";
-import { LAMP_CLASSES } from "./constants";
+import TimeBlock from "./components/TimeBlock/TimeBlock";
+import { LAMP_CLASSES, GRID_SIZES } from "./constants";
 import useClock from "./hooks/useClock";
 import { getBerlinClockData } from "./utils/BerlinClock";
 
 function App() {
   const time = useClock();
-  const { isSecondsEven } = getBerlinClockData(time);
+  const { isSecondsEven, fiveHoursCount, singleHoursCount } =
+    getBerlinClockData(time);
   return (
     <>
       <h1>Berlin Clock</h1>
       <div className="clock">
-        <section role="seconds" className="seconds">
+        <section role="seconds">
           <Lamp
             shape="rounded"
             litColor={isSecondsEven ? LAMP_CLASSES.YELLOW : ""}
           />
+        </section>
+        <section role="hours">
+          {[fiveHoursCount, singleHoursCount].map((_, index) => (
+            <TimeBlock
+              lamps={GRID_SIZES.FOUR}
+              litLamps={index === 0 ? fiveHoursCount : singleHoursCount}
+              litColor={LAMP_CLASSES.RED}
+            />
+          ))}
         </section>
       </div>
     </>
